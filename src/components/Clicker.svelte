c35ff07b3cec5f98e1b7ce1e16d05193eea5e89f
Winwin <em.ireri41@gmail.com>
2023-01-30 18:28:11 +0300

    Add files via upload

---
+++ src/components/Clicker.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    export let clicks: number = 0;
+  
+    function add() {
+      clicks += 1;
+    }
+  
+    function subtract() {
+      clicks -= 1;
+    }
+  </script>
+  
+  <div class="counter">
+    <button on:click={subtract}>-</button>
+    <pre>{ clicks }</pre>
+    <button on:click={add}>+</button>
+  </div>
